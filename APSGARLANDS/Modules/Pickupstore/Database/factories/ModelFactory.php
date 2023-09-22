a4fb6eef99f481825411ec13f01c30b636f879f8
Prabakaran V <prabakaranpbk@gmail.com>
2023-09-22 18:29:17 +0530

    Pickupstore seeder minor correction

--- APSGARLANDS/Modules/Pickupstore/Database/factories/ModelFactory.php
+++ APSGARLANDS/Modules/Pickupstore/Database/factories/ModelFactory.php
@@ -8,7 +8,7 @@ use Modules\Pickupstore\Entities\Pickupstore;
 $factory->define(Pickupstore::class, function (Faker\Generator $faker) {
         $name = ['Aishah', 'Ahmad', 'Zara','Rayyan', 'Muhammad'  ];
         $email = ['Aishah@gmail.com', 'Ahmad@gmail.com', 'Zara@gmail.com','Rayyan@gmail.com', 'Muhammad@gmail.com' ];
-        $phone=[60-16-9639865,+60-16-7864512,60-16-4567894,+60-16-7894564,+60-16-124581];
+        $phone=['60-16-9639865','60-16-7864512','60-16-4567894','60-16-7894564','60-16-124581'];
         $city=['Kuala Lumpur','Johor Bahru','Penang',' Selangor','Terengganu'];
         $state=['KUL','JHR','PNG','SGR','TRG'];
         $selectedCity = $faker->randomElement($city);
@@ -18,7 +18,7 @@ return [
     'name' => $faker->randomElement($name),
     'tagline' => $faker->catchPhrase,
     'email' => $faker->randomElement($email),
-    'phone' => $faker->randomElement($phone),
+    'phone' => '60-16-'.$faker->numberBetween(124500, 9999999),
     'address_1' => $faker->streetAddress,
     'address_2' => $faker->secondaryAddress,
     'city' => $selectedCity,
